207487303a206c9ea1629335fa1d0a538be59bb4
neil-araiz <neilbrianaraiz@gmail.com>
2025-05-12 22:40:23 +0800

    Fix Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react"
 
 import './index.css'
 import App from './App.jsx'
